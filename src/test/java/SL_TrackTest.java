f33d23e0ca28105d435a4acf33580be6a1e3977d
Milev.Sersei <domovenak2425@mail.ru>
2025-12-03 20:43:01 +0300

    Update SL_TrackTest.java

--- src/test/java/SL_TrackTest.java
+++ src/test/java/SL_TrackTest.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-public class SL_TrackTest {
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest {
+    @ParameterizedTest
+    @CsvFileSource(resources = "login_test.csv", numLinesToSkip = 1)
+    void testLogin01(String log, String pass) {
+        open("https://slqamsk.github.io/cases/slflights/v01/");
+        $x("//input[@id='username']").setValue(log);
+        $x("//input[@id='password']").setValue(pass);
+        $x("//button[@id='loginButton']").click();
+    }
 }
